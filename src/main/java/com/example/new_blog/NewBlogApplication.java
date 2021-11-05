38416db4814d60dfd6650d8552ce21947e545acd
zamaochick <2226221372@qq.com>
2021-11-05 09:27:39 +0800

    2021/11/5创建项目 每天一更新就是成功

---
+++ src/main/java/com/example/new_blog/NewBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_blog/NewBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
